6a32a1e2664889871911a05d2ec794d8a409be7a
alumno606 alumno606 <alumno606@labsis-a6m6.labsis.frc.utn.edu.ar>
2024-04-16 22:22:44 -0300

    Fin de clase

---
+++ 16-04/Desafio/index.js
@@ -0,0 +1,9 @@
+import seedrandom from "seedrandom";
+
+var random = seedrandom(1763519)
+
+let numerosAleatorios = Array.from({ length: 100}, () => random.int32())
+
+console.log("Demostraciones de join: {" + numerosAleatorios.join(", ") + "}")
+
+console.log("Cantidad de positivos", numerosAleatorios.filter(value => value > 0).length)
